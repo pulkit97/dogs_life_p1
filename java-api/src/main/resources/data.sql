14a1b3e15cff2201ef5763926bcff15b49be161b
Ceyda <cey.akbulut@gmail.com>
2023-07-24 16:20:30 +0100

    Sql training-24.07.2023

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,3 +1,7 @@
-INSERT INTO dogs (dog_id, name, age) VALUES (1, 'Einstein', 3);
-INSERT INTO dogs (dog_id, name, age) VALUES (2, 'Kaya', 5);
-INSERT INTO dogs (dog_id, name, age) VALUES (3, 'Lassie', 7);
+INSERT INTO OWNERS (Owner_ID, Name) VALUES (11, 111);
+INSERT INTO OWNERS (Owner_ID, Name) VALUES (12, 122);
+INSERT INTO OWNERS (Owner_ID, Name) VALUES (13, 133);
+
+INSERT INTO DOGS (ID, Name, Age, Owner_ID) VALUES (1, 'Einstein', 3, 11);
+INSERT INTO DOGS (ID, Name, Age, Owner_ID) VALUES (2, 'Kaya', 5, 12);
+INSERT INTO DOGS (ID, Name, Age, Owner_ID) VALUES (3, 'Lassie', 7, 13);
--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -1,8 +1,19 @@
-DROP TABLE IF EXISTS dogs;
+DROP TABLE IF EXISTS DOGS;
+DROP TABLE IF EXISTS OWNERS;
 
-CREATE TABLE dogs (
-    dog_id INT NOT NULL,
-    name VARCHAR(250) NOT NULL,
-    age INT NOT NULL
+CREATE TABLE OWNERS/* creates a new table called OWNERS*/
+(
+ Owner_ID INT NOT NULL  AUTO_INCREMENT  PRIMARY KEY, /*Add field id that is an integer that auto increments and PK*/
+ Name INT NOT NULL
+);
+
+
+CREATE TABLE DOGS /* creates a new table called dogs*/
+(
+ ID INT NOT NULL, /*Add field id that is an integer that auto increments and PK*/
+ Name VARCHAR(250) NOT NULL,  /*Add field name that is a none-null string*/
+ Age INT NOT NULL, /*Add field age that is a none-null integer*/
+ Owner_ID INT NOT NULL, /*Add field Owner_ID that is a none-null integer*/
+ FOREIGN KEY (Owner_ID) REFERENCES OWNERS(Owner_ID)
 );
 
