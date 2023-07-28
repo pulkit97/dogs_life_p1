bed961252eab728f6a4b540dd5b4493d85c53d2e
Pulkit <pulkitkhera97@gmail.com>
2023-07-28 15:05:21 +0100

    Added Swagger

--- superheroes/src/main/java/com/qa/superheroes/controller/HeroController.java
+++ superheroes/src/main/java/com/qa/superheroes/controller/HeroController.java
@@ -3,9 +3,8 @@ package com.qa.superheroes.controller;
 import com.qa.superheroes.model.Hero;
 import com.qa.superheroes.service.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +23,18 @@ public class HeroController {
     public List<Hero> getHeroesStartingWithLetter(@PathVariable String letter){
         return heroService.getHeroesStartingWithLetter(letter);
     }
+
+    @PostMapping("/heroes/{letter}")
+    public HttpStatus saveNewHero(@RequestBody Hero hero){
+        Hero result = heroService.saveHero(hero);
+
+        if  (result == null){
+            return HttpStatus.PRECONDITION_FAILED;
+        }
+        else {
+            return HttpStatus.CREATED;
+        }
+    }
+
 
 }
