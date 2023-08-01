52b7442b504a504f4d02456a1976400e025c4ba0
Pulkit <pulkitkhera97@gmail.com>
2023-08-01 11:20:26 +0100

    Another commit

--- ReactPractice/superheroes-ui/src/index.js
+++ ReactPractice/superheroes-ui/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -32,7 +33,9 @@ export default images;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
